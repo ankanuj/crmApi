45e9d10ba668cc505b0d782278c81bf12e392388
ankanuj <s.singhanuj132@gmail.com>
2021-11-23 11:28:54 +0530

    first commit of restapi

---
+++ api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class enquiryForm(models.Model):
+  fullName = models.CharField(max_length=200, blank=False)
+  email = models.EmailField(blank=False)
+  mobile = models.IntegerField(blank=False)
+  courseInterest = models.CharField(max_length=150, blank=False)
+  city = models.CharField(max_length=100, blank=False)
+  state = models.CharField(max_length=100, blank=False)
+  zipcode = models.IntegerField(blank=False)
+  dateCreated = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+  def __str__(self):
+    return self.fullName
